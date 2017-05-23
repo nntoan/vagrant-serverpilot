46296398f32cf8b31dfd3da5bf4429763d2b771b
Toan Nguyen <toan.nguyen@balanceinternet.com.au>
2017-05-23 10:59:08 +0700

    Initial commit

---
+++ Vagrantfile
@@ -0,0 +1,88 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+require 'yaml'
+require 'fileutils'
+
+# Vagrantfile API/syntax version. Don't touch unless you know what you're doing!
+VAGRANTFILE_API_VERSION = "2"
+
+# Path to settings file
+SETTINGS_FILE = "settings.yml"
+
+# Load the settings file with fucking absolute path for the greater good
+settings = YAML.load_file(File.join(File.dirname(__FILE__), SETTINGS_FILE))
+
+# Function to check whether VM was already provisioned
+def provisioned?(vm_name='default', provider='virtualbox')
+  File.exist?(".vagrant/machines/#{vm_name}/#{provider}/action_provision")
+end
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+  begin
+    config.vm.box = settings['vagrant']['box'] ||= 'ubuntu/xenial64'
+    if settings['vagrant']['box_url'] != nil
+      config.vm.box_url = settings['vagrant']['box_url']
+    end
+
+    # After provisioned, use 'serverpilot' user instead
+    #config.ssh.username = settings['vagrant']['ssh']['username'] if provisioned?
+
+    # Configure default synced folder (disable by default) - WIP
+    config.vm.synced_folder '.', '/vagrant', disabled: true
+
+    config.vm.provider "virtualbox" do |vbox, override|
+      # IPs and ports handler
+      settings['network']['ports'].each do |port|
+        override.vm.network "forwarded_port", guest: port['guest'], host: port['host']
+      end
+      if settings['network']['private']['enabled'] == true
+        override.vm.network "private_network", ip: settings['network']['private']['ip_addr']
+      end
+      if settings['network']['public']['enabled'] == true
+        override.vm.network "public_network", bridge: settings['network']['public']['bridge'], ip: settings['network']['public']['ip_addr']
+      end
+
+      # Name of machine
+      vbox.name = settings['vbox']['name'] ||= 'demobox2'
+      # To debug only
+      vbox.gui = settings['vbox']['debug'] ||= false
+
+      # Customize the amount of memory & cpus on the VM
+      if settings['vbox']['res_auto'] == true
+        host = RbConfig::CONFIG['host_os']
+        if host =~ /darwin/
+          cpus = `sysctl -n hw.ncpu`.to_i
+          mem = `sysctl -n hw.memsize`.to_i / 1024 / 1024 / 4
+        elsif host =~ /linux/
+          cpus = `nproc`.to_i
+          mem = `grep 'MemTotal' /proc/meminfo | sed -e 's/MemTotal://' -e 's/ kB//'`.to_i / 1024 / 4
+        else
+          cpus = settings['vbox']['vcpu'] ||= 2
+          mem = settings['vbox']['ram'] ||= 1536
+        end
+      else
+        cpus = settings['vbox']['vcpu'] ||= 1
+        mem = settings['vbox']['ram'] ||= 1024
+      end
+      vbox.customize ["modifyvm", :id, "--memory", mem]
+      vbox.customize ["modifyvm", :id, "--cpus", cpus]
+    end
+
+    # There is known bug of ubuntu xenial64 cloud-image, it's using password instead of private_key
+    # Here is how we deal with it, for faster "vagrant up"
+    config.vm.provision "shell", inline: <<-SHELL
+       echo "ubuntu:ubuntu" | sudo chpasswd
+    SHELL
+
+    # If you are going to add more opts, ensure " \
+    # after the last opt, e.g -x #{settings['hello']['world']}" \
+    # and the args must be 1 letter, because we're using getopts
+    config.vm.provision "shell", path: "scripts/bootstrap.sh",
+    args: "-i #{settings['serverpilot']['server_id']} \
+           -k #{settings['serverpilot']['api_key']} \
+           -s #{settings['serverpilot']['sudo']} \
+           -p #{settings['serverpilot']['php']} \
+           -b #{settings['shell']['ohmybash']} \
+           -z #{settings['shell']['ohmyzsh']}" \
+  end
+end
---
+++ scripts/bootstrap.sh
@@ -0,0 +1,110 @@
+#!/usr/bin/env bash
+# ------------------------------------------------------------------------------
+# FILE: bootstrap.sh
+# DESCRIPTION: Bootstrap script to install your ServerPilot boxes
+# AUTHOR: Toan Nguyen (destro [at] nnt [dot] com)
+# VERSION: 1.0.0
+# ------------------------------------------------------------------------------
+set -e
+
+while getopts "i:k:s:p:b:z:" opt; do
+    case "$opt" in
+        i)
+          server_id="$OPTARG" ;;
+        k)
+          api_key="$OPTARG" ;;
+        s)
+          sudo_nopasswd="$OPTARG" ;;
+        p)
+          default_php="$OPTARG" ;;
+        b)
+          install_omb="$OPTARG" ;;
+        z)
+          install_omz="$OPTARG" ;;
+    esac
+done
+
+# Default user vars
+user="ubuntu"
+homedir=$(getent passwd $user | cut -d ':' -f6)
+
+# SP user vars
+sp_user="serverpilot"
+sp_homedir=$(getent passwd $sp_user | cut -d ':' -f6)
+sp_sudoers="/etc/sudoers.d/$sp_user"
+sp_agentlog="/var/log/serverpilot/agent.log"
+sp_installer="serverpilot-installer"
+
+spinner()
+{
+  local pid=$1
+  local delay=0.75
+  local spinstr='|/-\'
+  while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+    local temp=${spinstr#?}
+    printf " [%c]  " "$spinstr"
+    local spinstr=$temp${spinstr%"$temp"}
+    sleep $delay
+    printf "\b\b\b\b\b\b"
+  done
+  printf "    \b\b\b\b"
+}
+
+sp_tweak()
+{
+  sudo killall tail &>/dev/null
+  sudo cp /root/.my.cnf "$sp_homedir"/.my.cnf &>/dev/null
+  sudo chown $sp_user:$sp_user "$sp_homedir"/.my.cnf &>/dev/null
+  sudo cp "$homedir"/.ssh/authorized_keys "$sp_homedir"/.ssh/authorized_keys &>/dev/null
+  sudo chown $sp_user:$sp_user "$sp_homedir"/.ssh/authorized_keys &>/dev/null
+  if [[ "$sudo_nopasswd" == true ]]; then
+    echo "serverpilot ALL=(ALL) NOPASSWD:ALL" | sudo tee "$sp_sudoers" && sudo chmod 440 "$sp_sudoers" &>/dev/null
+    echo "env bash'" | sudo tee "$sp_homedir"/.bash_profile &>/dev/null
+    sudo chown $sp_user:$sp_user "$sp_homedir"/.bash_profile
+  fi
+  # Set default PHP version
+  echo "Setting default PHP version..."
+  echo "sp-php-cli sp-php-cli/default_php_version select $default_php" | sudo debconf-set-selections
+  sudo dpkg-reconfigure -f noninteractive sp-php-cli
+  # WIP - not working, don't know why.
+  if [[ "$install_omb" == true && "$install_omz" != true ]]; then
+    sudo -u $sp_user bash -c "$(curl -fsSL https://raw.github.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+  fi
+  if [[ "$install_omz" == true && "$install_omb" != true ]]; then
+    sudo apt-get -qq -y install zsh
+    sudo -u $sp_user bash -c "$(wget https://raw.github.com/nntoan/oh-my-zsh-custom/master/install.sh -O -)"
+  fi
+}
+
+if [[ ! -f "$sp_installer" && ! -f "$homedir"/.sp_done ]]; then
+  sudo apt-get update && sudo apt-get -y install ca-certificates wget
+  sudo wget -nv -O "$sp_installer" https://download.serverpilot.io/serverpilot-installer
+  sudo bash "$sp_installer" --server-id="$server_id" --server-apikey="$api_key"
+fi
+
+if [[ -f "$sp_agentlog" && ! -f "$homedir"/.sp_assume_ins && ! -f "$homedir"/.sp_done ]]; then
+  echo
+  echo "********************************************************************************"
+  echo "Resume installing ServerPilot packages..."
+  echo "Please be patient..."
+  echo
+  ( sudo tail -f "$sp_agentlog" & ) | grep -q "ACTION INFO: Performing action: enable_ssh_password_auth" && echo "$(date +%s)" > "$homedir"/.sp_assume_ins
+  sudo chown $user:$user "$homedir"/.sp_assume_ins
+fi
+
+if [[ -f "$homedir"/.sp_assume_ins && ! -f "$homedir"/.sp_done ]]; then
+  sp_homedir=$(getent passwd $sp_user | cut -d ':' -f6)
+  ( sp_tweak ) &
+  echo "Start tweaking some configs for your ServerPilot bundle..."
+  spinner $!
+  echo " OK"
+  echo "$(date +%s)" > "$homedir"/.sp_done
+  sudo chown $user:$user "$homedir"/.sp_done
+  echo "********************************************************************************"
+  echo
+  echo "Your balance-vagrant is ready! Log in with: "
+  echo "| - ssh ubuntu@<your_private_ip_addr> -oStrictHostKeyChecking=no -A"
+  echo "Your machine credentials: "
+  echo "| - username: ubuntu"
+  echo "| - password: ubuntu"
+fi
